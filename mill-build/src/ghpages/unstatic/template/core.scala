f3197f008ac7a4d662581a0c0ab78f4c0bcda203
Steve Waldman <swaldman@mchange.com>
2026-01-20 00:08:22 -0300

    Initial import.

---
+++ mill-build/src/ghpages/unstatic/template/core.scala
@@ -0,0 +1,22 @@
+package ghpages.unstatic.template
+
+case class Params(
+  siteServerAbsUrl      : String,
+  title                 : String,
+  maxFrontPageEntries   : Int    = 5,
+  timeZone              : String = "America/NewYork",
+  sitePath              : String = "/",
+  sitePackage           : String = "my.unstatic.website",
+  millVersion           : String = "1.0.6",
+  millJvmVersion        : String = "17",
+  untemplateMillVersion : String = "0.3.0",
+  unstaticVersion       : String = "0.4.0",
+  scalaVersion          : String = "3.3.7",
+  entriesSubpackage     : String = "entries",
+  entriesDir            : String = "entries",
+):
+  val entriesPackage =
+    if entriesSubpackage.length > 0 then s"${sitePackage}.${entriesSubpackage}" else s"${sitePackage}"
+
+  val basePathExpression =
+    if sitePath.isBlank() || sitePath == "/" then "Rooted.root" else s"""Rooted( "${sitePath}" )"""
---
+++ mill-build/src/trivialtemplate/core.scala
@@ -0,0 +1,26 @@
+// stolen from feedletter.
+// factor into a library at some point.
+
+package trivialtemplate
+
+/**
+  * Replacement is case-insensitive
+  */
+object TrivialTemplate:
+  private val KeyRegex        = """(?i)(?<!\\)\%(\w+)\%""".r
+  private val EscapedKeyRegex = """(?i)\\+\%\w+\%""".r
+
+  object Defaults:
+    val Fail : String => String = k => throw new KeyNotFound(s"Key '$k' required by template, not found.")
+    val AsIs : String => String = k => s"%$k%"
+
+  def escapeText( text : String ) : String =
+    KeyRegex.replaceAllIn( text, m => """\""" + m.group(0) )
+
+case class TrivialTemplate( body : String ):
+  import TrivialTemplate.*
+
+  def resolve( replacements : Map[String,String], defaults : String => String = Defaults.Fail ) : String =
+    val replaced = KeyRegex.replaceAllIn( body, m => { val k = m.group(1).toLowerCase(); replacements.map((k,v)=>(k.toLowerCase,v)).getOrElse( k, defaults(k) ) } )
+    val unescaped = EscapedKeyRegex.replaceAllIn( replaced, m => m.group(0).substring(1) ) 
+    unescaped
